3504827396abdfff32be79c004b68a2f81be4df8
Nicole Maldonado <melomc02@gmail.com>
2016-06-02 20:37:55 -0400

    Consulta de Historias de Usuario , Integracion de Tabla con consulta e Interfaz de Historia de Usuario

---
+++ src/userstory/ConsultasUserStory.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userstory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import scrumproject.Conexion;
+
+
+/**
+ *
+ * @author Nicole
+ */
+public class ConsultasUserStory {
+    
+    private final Conexion conexion;
+
+    public ConsultasUserStory() throws ClassNotFoundException, SQLException {
+        conexion = new Conexion();
+    }
+
+    public void guardarHistoria(UserStory story) throws SQLException {
+        Statement instancia;
+        instancia = conexion.getInstancia();
+        String titulo = story.getTitulo();
+        String despripcion = story.getDescripcion();
+        String criteriosDeAceptacion = story.getCriterios();
+        int complejidad = story.getComplejidad();
+        int prioridad = story.getPrioridad();
+        String query;
+        query = String.format("INSERT INTO historiadeusuario (titulo,descripcion,criteriosAceptacion,complejidad,prioridad) VALUES(\"%s\",\"%s\",\"%s\",%d,%d)",titulo,despripcion,criteriosDeAceptacion,complejidad,prioridad);
+        int result = instancia.executeUpdate(query);
+        System.out.println(result);
+    }
+    
+}
