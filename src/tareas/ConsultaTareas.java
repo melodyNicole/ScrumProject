6d44ab3671ee842a0bcae7663307dff36c955804
Carla <carly.cc73@gmail.com>
2016-06-04 21:01:57 -0500

    la consulta para guardar la tarea

---
+++ src/tareas/ConsultaTareas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareas;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import scrumproject.Conexion;
+
+/**
+ *
+ * @author Carla
+ */
+ 
+public class ConsultaTareas {
+     private final  Conexion conexion;
+     public ConsultaTareas()throws ClassNotFoundException, SQLException { 
+         conexion=new Conexion();
+     }
+      public void guardarTarea(Tarea tarea) throws SQLException
+      {
+       Statement instancia;
+        instancia = conexion.getInstancia();
+        String titulo = tarea.getTitulo();
+        String despripcion = tarea.getDescripcion();
+        String query;
+        query = String.format("INSERT INTO tarea (titulo,descripcion) VALUES(\"%s\",\"%s\")",titulo,despripcion);
+        int result = instancia.executeUpdate(query);
+        if(result==1) JOptionPane.showMessageDialog(null," TAREA GUARDADA");
+      }
+}
